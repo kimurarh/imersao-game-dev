624d68b73d821f74b1fffc63a04c8dfe5fa17305
Rafael Kimura <rafaelkimura94@gmail.com>
2020-06-23 13:57:20 -0300

    day 2

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,59 +1,41 @@
-class Personagem {
-    constructor(imagem) {
-        this.imagem = imagem;
-        
-        this.matriz = [];
-        let xAxis = 0;
-        let yAxis = 0;
-        
-        // Gera a matriz com frames do personagem
-        for(var c = 0; c <= 15; c++ ) {
-            if(c % 4 === 0 && c > 0) {
-                xAxis = 0;
-                yAxis += 270;
-            } 
-            this.matriz[c] = [xAxis, yAxis];
-            xAxis += 220;
-        }
+class Personagem extends Animacao {
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
 
-        // For using the dog sprites
-        // for(var c = 0; c <= 20; c++ ) {
-        //     if(c % 3 === 0 && c > 0) {
-        //         xAxis = 0;
-        //         yAxis += 203;
-        //     } 
-        //     this.matriz[c] = [xAxis, yAxis];
-        //     xAxis += 300;
-        // }
-
-
-        this.frameAtual = 0;
+        this.yInicial = height - this.altura;
+        this.y = this.yInicial;
+        this.velocidadeDoPulo = 0;
+        this.gravidade = 3;
     }
 
-    exibe() {
-        // Coordenadas X e Y no canvas (onde o personagem irá aparecer)
-        let xCoord = 0;   
-        let yCoord = height - 154;    // Obs.: height -> altura do canvas 135
-        // Largura e altura da imagem no canvas
-        let imgLarg = 110;  //200
-        let imgAlt = 135;    // 154
-        // posição (X e Y) do frame do personagem dentro da imagem
-        let xFrame = this.matriz[this.frameAtual][0];   
-        let yFrame = this.matriz[this.frameAtual][1]; 
-        // Largura e altura do frame do personagem
-        let xFrameLarg = 220;  // 300 
-        let yFrameAlt = 270;    // 203
-
-        image(this.imagem, xCoord, yCoord, imgLarg, imgAlt, xFrame, yFrame, xFrameLarg, yFrameAlt);	
-        
-        this.anima();
+    pula() {
+        this.velocidadeDoPulo = -33;
     }
 
-    anima() {
-        this.frameAtual++;
+    aplicaGravidade() {
+        this.y += this.velocidadeDoPulo;
+        this.velocidadeDoPulo += this.gravidade;
 
-        if(this.frameAtual >= this.matriz.length - 1) {
-            this.frameAtual = 0;
+        if(this.y > this.yInicial) {
+            this.y = this.yInicial;
         }
     }
+
+    estaColidindo(inimigo) {
+        const precisao = 0.7;
+        const colisao = collideRectRect(this.x, this.y, this.largura * precisao, this.altura * precisao, inimigo.x, inimigo.y, inimigo.largura * precisao, inimigo.altura * precisao);
+
+        return colisao;
+    }
+
+    // // Atualiza o frame do personagem
+    // this.xAxis += this.imagem.width/3;  // nº frames na horizontal da imagem
+
+    // if(this.xAxis >= this.imagem.width) {
+    //     this.xAxis = 0;
+    //     this.yAxis += this.imagem.height/7; // nº frames na vertical da imagem
+    //     if(this.yAxis >= this.imagem.height) {
+    //         this.yAxis = 0;
+    //     }
+    // }
 }
